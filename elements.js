7622788aef19aa4971f481132d7776759e9e33bf
AndreyFufligin <113243927+AndreyFufligin@users.noreply.github.com>
2023-05-14 13:31:38 +0300

    Add files via upload

---
+++ elements.js
@@ -0,0 +1,10 @@
+const ELEMENTS = {
+  FORMS: document.querySelectorAll('.AddTask'),
+  TEXT_HIGH_INPUT: document.getElementById('highInput'),
+  TEXT_LOW_INPUT: document.getElementById('lowInput'),
+  BTN: document.querySelectorAll('.BtnADD'),
+  HIGH_LIST: document.querySelector('.todosListHigh'),
+  LOW_LIST: document.querySelector('.todoslistLow'),
+};
+
+export { ELEMENTS };
